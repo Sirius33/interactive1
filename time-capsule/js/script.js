ae441ab6e955b31f68bbfe934e0fa0cc0c65fb99
Sirius33 <siruichang@cca.edu>
2019-04-11 12:51:22 -0700

    4.11

---
+++ time-capsule/js/script.js
@@ -0,0 +1,60 @@
+
+    // $('document').ready(function(){
+    //   $(window).onclick(function(){
+    //     $('body').toggleClass('colorful');  
+    //   });
+    // });
+//  $('a').mouseover(function() {
+//     $('.description1,description2').toggle();
+// });
+$('p1').click(function() {
+    $('.hided1').toggle();
+});
+$('p2').click(function() {
+    $('.hided2').toggle();
+});
+$('p3').click(function() {
+    $('.hided3').toggle();
+});
+$('p4').click(function() {
+    $('.hided4').toggle();
+});
+$('p5').click(function() {
+    $('.hided5').toggle();
+});
+$('p6').click(function() {
+    $('.hided6').toggle();
+});
+$('p7').click(function() {
+    $('.hided7').toggle();
+});
+$('p8').click(function() {
+    $('.hided8').toggle();
+});
+$('p9').click(function() {
+    $('.hided9').toggle();
+});
+$('p10').click(function() {
+    $('.hided10').toggle();
+});
+$('p11').click(function() {
+    $('.hided11').toggle();
+});
+$('p12').click(function() {
+    $('.hided12').toggle();
+});
+$('p13').click(function() {
+    $('.hided13').toggle();
+});
+$('p14').click(function() {
+    $('.hided14').toggle();
+});
+$('p15').click(function() {
+    $('.hided15').toggle();
+});
+$('p16').click(function() {
+    $('.hided16').toggle();
+});
+$('p17').click(function() {
+    $('.hided17').toggle();
+});
---
+++ watergif/js/script.js
@@ -0,0 +1,8 @@
+
+    $('document').ready(function(){
+      $(window).click(function(){
+        $('.wave').toggleClass('bg'); 
+        $('.facechange').toggleClass('show'); 
+
+      });
+    });
